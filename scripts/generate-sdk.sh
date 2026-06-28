a8b7c5a93ad04186fe54b51270624bd7258cd389
Shiron <contact@shiron.io>
2026-06-28 04:05:17 +0200

    Initial Commit

---
+++ scripts/generate-sdk.sh
@@ -0,0 +1,1 @@
+kiota generate -l CSharp -c NginxProxySdk -n NginxProxy.Sdk -d http://127.0.0.1:81/api/schema -o ./src/generated/NginxProxySdk
---
+++ src/Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using Shiron.ComposeToNginx.Cli.Commands;
+using Spectre.Console.Cli;
+
+var app = new CommandApp();
+app.Configure(c => {
+    c.SetApplicationName("compose-to-nginx");
+    c.SetApplicationVersion("1.0.0");
+
+    c.AddCommand<EchoCommand>("echo");
+});
+
+await app.RunAsync(args);
